35c142b8c294ceec1c5f906a57895ff5e292d88d
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2024-06-13 14:05:24 -0400

    Deleting the example function.

--- R/HLA_column_repair.R
+++ R/HLA_column_repair.R
@@ -1,5 +1,22 @@
-# HLA_column_repair function. This function will change column names that have the official HLA nomenclature (e.g. "HLA-A*" or "HLA-A") to a format supported by R (e.g. "HLA_A"). The dash and asterisk are a special characters in R, and makes selecting columns by name difficult.
+#' @title HLA_column_repair
+#'
+#' @description This function will change column names that have the official
+#'  HLA nomenclature (e.g. "HLA-A*" or "HLA-A") to a format supported by R
+#'  (e.g. "HLA_A"). The dash and asterisk are a special characters in R,
+#'  and makes selecting columns by name difficult.
+#'
+#' @param data A data frame
+#'
+#' @return A data frame object with column names renamed in the "HLA_A" format.
+#'
+#' @examples
+#' data(toydata) # I think we need to make some toydata.
+#' output <- HLA_column_repair(toydata)
+#' @export
+#' @importFrom dplyr "%>%"
 
 HLA_column_repair <- function(data) {
-  data %>% rename_with(~ str_replace(., "HLA\\-", "HLA_")) %>% rename_with(~ str_replace(., "\\*$", ""))
+  data %>%
+    rename_with(~ str_replace(., "HLA\\-", "HLA_")) %>%
+    rename_with(~ str_replace(., "\\*$", ""))
 }
