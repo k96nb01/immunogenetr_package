61e95ff66b34de17fa7e06e114af245f70e9fa80
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2024-06-21 16:23:36 -0400

    Updated description and corrected spelling in some of the comments for HLA_column_repair.

--- R/HLA_column_repair.R
+++ R/HLA_column_repair.R
@@ -1,9 +1,11 @@
 #' @title HLA_column_repair
 #'
 #' @description This function will change column names that have the official
-#'  HLA nomenclature (e.g. "HLA-A*" or "HLA-A") to a format supported by R
-#'  (e.g. "HLA_A"). The dash and asterisk are a special characters in R,
-#'  and makes selecting columns by name difficult.
+#'  HLA nomenclature (e.g. "HLA-A*" or "HLA-A") to a format more easily selected
+#'  in tidyverse function (e.g. "HLA_A"). The dash and asterisk are a special
+#'  characters in R, and makes selecting columns by name difficult. This function
+#'  will also allow for easily changing back to WHO-compliant nomenlcautre
+#'  (e.g. "HLA-A*").
 #'
 #' @param data A data frame
 #'
@@ -16,7 +18,7 @@
 #' @importFrom dplyr "%>%"
 
 HLA_column_repair <- function(data, format = "tidyverse", asterisk = FALSE) {
-  # Step 1: turn "HLA-A" to "HLA_A" or vice-versa.
+  # Step 1: turn "HLA-A" to "HLA_A" or vice versa.
   if (format == "tidyverse") {
     step_1 <- data %>% rename_with(~ str_replace(., "HLA\\-", "HLA_"))
   } else if (format == "WHO") {
@@ -26,7 +28,7 @@ HLA_column_repair <- function(data, format = "tidyverse", asterisk = FALSE) {
   }
   # Step 2: remove asterisk from all columns.
   step_2 <- step_1 %>% rename_with(~ str_replace(., "\\*$", ""))
-  # Step 3: add askterisk back if wanted.
+  # Step 3: add asterisk back if wanted.
   if (asterisk == TRUE) {
     step_3 <- step_2 %>% rename_with(~ str_replace(., "$", "*"), starts_with("HLA"))
   } else {
