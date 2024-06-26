2f0a1e90ec1dbcbff85722b7badc10cf240f7726
coskun <coskun@coskuns-MacBook-Pro.local>
2024-06-26 18:25:42 -0400

    added HLA_prefix_add

---
+++ R/HLA_prefix_add.R
@@ -0,0 +1,29 @@
+#' @title HLA_prefix_add
+#'
+#' @description This function adds a specified prefix to the beginning of
+#' values in the specified columns of the given data frame.
+#'
+#' @param .data A data frame
+#' @param columns Name of columns in .data to which the prefix should be added
+#' @param prefix A character string to be added as a prefix to the column values.
+#' Default is "HLA-".
+#'
+#' @return A data frame with the specified prefix added to the values in the
+#' selected columns.
+#'
+#' @examples
+#' data(toydata)
+#' output <- toydata %>% HLA_prefix_add(A.1, "A")
+#'
+#' @export
+#'
+#' @importFrom dplyr mutate
+#' @importFrom dplyr across
+#' @importFrom dplyr %>%
+#' @importFrom stringr str_replace
+#' @importFrom stringr str_interp
+
+HLA_prefix_add <- function(.data, columns, prefix = "HLA-") {
+  # Add string to beginning of typing
+  .data %>% mutate(across({{ columns }}, ~str_replace(., "^", str_interp('${ prefix }'))))
+}
--- R/HLA_prefix_remove.R
+++ R/HLA_prefix_remove.R
@@ -6,14 +6,14 @@
 #' `c("2", "2", "11:01", "66", "15:01")`
 #'
 #' @param .data A data frame
-#' @param columns Names of columns in `.data` containing HLA typing results.
+#' @param columns Names of columns in .data containing HLA typing results
 #'
 #' @return A data frame object with specified columns modified to remove HLA
 #' and locus prefixes.
 #'
 #' @examples
 #' data(toydata)
-#' output <- toydata %>% HLA_prefix_remove(Column1)
+#' output <- toydata %>% HLA_prefix_remove(A.2_recipient)
 #'
 #' @export
 #'
