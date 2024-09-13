3e50782569ee071a1824378590df7aca68222d37
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2024-09-13 12:25:07 -0400

    Rewrote HLA_truncate

--- R/HLA_truncate.R
+++ R/HLA_truncate.R
@@ -1,24 +1,27 @@
 #' @title HLA_truncate
 #'
-#' @description This function truncates the HLA typing values. The truncation
-#' is based on the number of fields specified and optionally retains any
-#' WHO-recognized suffixes (L, S, C, A, Q, or N). Note this will not keep
-#' G or P group designations, as these are defined to specific fields
-#' (3 fields for G groups, and 2 fields for P groups), so if typings
-#' are to be truncated, the G or P group nomenclature is not being preserved.
+#' @description This function truncates HLA typing values in molecular nomenclature
+#' (for example from 4 fields to 2 fields). The truncation is based on the number
+#' of fields specified and optionally retains any WHO-recognized suffixes
+#' (L, S, C, A, Q, or N) or G and P group designations (G or P). This function
+#' will work on individual alleles (e.g. "HLA-A*02:01:01:01") or on all alleles
+#' in a GL string (e.g. "HLA-A*02:01:01:01+HLA-A*68:01:01^HLA-DRB1*01:01:01+HLA-DRB1*03:01:01").
 #'
-#' @param data A string containing an HLA type.
+#'
+#' @param data A string containing an HLA allele or a GL string.
 #' @param fields An integer specifying the number of fields to retain in the
 #' truncated values. Default is 2.
 #' @param keep_suffix A logical value indicating whether to retain any
 #' WHO-recognized suffixes. Default is TRUE.
+#' @param keep_G_P_group A logical value indicated whether to retain any
+#' G or P group desingations. Default is FALSE.
 #'
 #' @return A string with the HLA typing truncated according to
 #' the specified number of fields and optional suffix retention.
 #'
 #' @examples
 #' typing <- "A*01:01:01:02N"
-#' HLA_truncate(typing, fields = 2, keep_suffix = FALSE) # "A*01:01"
+#' HLA_truncate(typing) # "A*01:01"
 #'
 #' @export
 #'
@@ -30,42 +33,54 @@
 #' @importFrom stringr str_c
 #' @importFrom tidyr replace_na
 #' @importFrom dplyr na_if
+#' @importFrom tidyr separate_wider_delim
+#' @importFrom tidyr unite
 
-HLA_truncate <- function(data, fields = 2, keep_suffix = TRUE) {
-  # Split the input GL string into individual alleles
-  alleles <- strsplit(data, "\\^")[[1]]
+HLA_truncate <- function(data, fields = 2, keep_suffix = TRUE, keep_G_P_group = FALSE) {
+  # Don't alter data if it is not in molecular nomenclature.
+  if (!str_detect(data, "\\*")) {
+    return(data)
+  } else {
 
-  # Initialize an empty vector to store truncated alleles
-  truncated_alleles <- vector("character", length(alleles))
+    # Expand the GL string
+    alleles <- GLstring_expand_longer(data) %>%
+      # Extract any WHO-recognized suffixes
+      mutate(suffix = replace_na(str_extract(value, "(?<=[:digit:])[LSCAQNlscaqn]$"), "")) %>%
+      # Extract any P or G group designation
+      mutate(GP = replace_na(str_extract(value, "(?<=[:digit:])[PGpg]$"), "")) %>%
+      # Separate locus from allele, and separate alleles into discrete fields
+      separate_wider_delim(value, delim = "*", names = c("name", "type")) %>%
+      separate_wider_delim(type, delim = ":", names = c("one", "two", "three", "four"), too_few = "align_start") %>%
+      # Keep only numbers in each field, in case there were non-standard suffixes.
+      mutate(across(one:four, ~str_extract(., "[:digit:]+")))
 
-  # Loop through each allele to apply truncation
-  for (i in seq_along(alleles)) {
-    allele <- alleles[i]
-
-    # Extract fields based on the number of fields specified
-    A <- if (fields == 3) {
-      str_extract(allele, "(HLA-)?([:alnum:]{0,4})(\\*)?[:digit:]{1,4}:?[:digit:]{0,4}:?[:digit:]{0,4}")
+    # Delete fields for truncating and reunite the alleles
+    if (fields == 1) {
+      trunctated <- alleles %>% select(-four, -three, -two) %>% unite(final, name:one, sep = "*")
     } else if (fields == 2) {
-      str_extract(allele, "(HLA-)?([:alnum:]{0,4})(\\*)?[:digit:]{1,4}:?[:digit:]{0,4}")
-    } else if (fields == 1) {
-      str_extract(allele, "(HLA-)?([:alnum:]{0,4})(\\*)?[:digit:]{1,4}")
+      trunctated <- alleles %>% select(-four, -three) %>% unite(fields, one:two, sep = ":", na.rm = TRUE) %>% unite(final, name:fields, sep = "*")
+    } else if (fields == 3) {
+      trunctated <- alleles %>% select(-four) %>% unite(fields, one:three, sep = ":", na.rm = TRUE) %>% unite(final, name:fields, sep = "*")
     } else {
-      str_extract(allele, "(HLA-)?([:alnum:]{0,4})(\\*)?[:digit:]{1,4}:?[:digit:]{0,4}:?[:digit:]{0,4}:?[:digit:]{0,4}")
+      trunctated <- alleles %>% unite(fields, one:four, sep = ":", na.rm = TRUE) %>% unite(final, name:fields, sep = "*")
     }
 
-    # Extract any WHO-recognized suffixes
-    B <- replace_na(str_extract(allele, "[LSCAQNlscaqn]$"), "")
-
-    # Glue truncated typing and suffixes if indicated
-    if (keep_suffix == TRUE) {
-      truncated_alleles[i] <- na_if(str_c({A}, {B}, sep = ""), "NA")
+    # Retain suffix if desired
+    if (keep_suffix) {
+      with_suffix <- trunctated %>% unite(final, final, suffix, sep = "")
     } else {
-      truncated_alleles[i] <- A
+      with_suffix <- trunctated %>% select(-suffix)
     }
+    # Retain P/G group designation if desired
+    if (keep_G_P_group) {
+      with_g_p <- with_suffix %>% unite(final, final, GP, sep = "")
+    } else {
+      with_g_p <- with_suffix %>% select(-GP)
+    }
+
+    # Combine everything back to a GL string.
+    final <- with_g_p %>% rename(value = final) %>% ambiguity_table_to_GLstring()
+
+    return(final)
   }
-
-  # Join the truncated alleles back into a single string
-  truncated_string <- paste(truncated_alleles, collapse = "^")
-
-  return(truncated_string)
 }
