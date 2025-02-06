6c0f5f7594a5e15cc38b6ebb858a0cc0cf522e2e
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2025-02-06 13:46:44 -0500

    Added a file for the data.

---
+++ data.R
@@ -0,0 +1,16 @@
+#' HLA data for immunogenetr
+#'
+#' test1: HLA typing results of 10 individuals.
+#' test2: 100 rows of alleles from the HLA dictionary.
+#' test3: 10 rows of HLA haplotypes.
+#' test4: 10 rows of HLA typing data as found in a laboratory information system.
+#' test5: 10 rows of HLA typing data in mixed serologic/molecular format as found
+#' in a laboratory information system.
+#'
+#' @docType data
+#'
+#' @usage data(test1)
+#'
+#' @format tibble
+#'
+#' @keywords datasets
