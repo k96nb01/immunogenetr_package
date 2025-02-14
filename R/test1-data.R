58bd60b61c114db083ef1c9b2e5849bcf1f8b749
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2025-02-14 13:33:16 -0500

    Added file for first data set

---
+++ R/test1-data.R
@@ -0,0 +1,14 @@
+#' HLA data for immunogenetr
+#'
+#' test1: HLA typing results of 10 individuals.
+#'
+#' @docType data
+#'
+#' @usage data(test1)
+#'
+#' @format A tibble
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(test1)
