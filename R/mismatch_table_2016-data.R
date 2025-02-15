d2cc2eb1b3fdac4101d1b5bafacf64f0363fbfad
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2025-02-15 16:47:04 -0500

    Documentation for mismatch_table_2016 data

---
+++ R/mismatch_table_2016-data.R
@@ -0,0 +1,23 @@
+#' Consensus mismatch numbers for every possible allele combination
+#' at a single locus, from the 2016 publication.
+#'
+#' @docType data
+#'
+#' @usage data(mismatch_table_2016)
+#'
+#' @format
+#' A tibble:
+#' \describe{
+#'   \item{Patient:Donor}{allele combinations for patient and donor}
+#'   \item{#GvH:#Max}{consensus mismatch number for each direction}
+#'     ...
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source <https://www.nature.com/articles/bmt2010132>
+#'
+"mismatch_table_2016"
+
+
+
