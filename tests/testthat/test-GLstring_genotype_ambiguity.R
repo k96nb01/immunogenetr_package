13b59b700455a45c46b43833268d9053e496ca7d
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2025-02-16 13:18:44 -0500

    Updated package with styler

--- tests/testthat/test-GLstring_genotype_ambiguity.R
+++ tests/testthat/test-GLstring_genotype_ambiguity.R
@@ -5,10 +5,14 @@ library(stringr)
 test_that("GLstring_genotype_ambiguity correctly retains the first genotype ambiguity", {
   HLA_type <- data.frame(
     sample = c("sample1", "sample2"),
-    HLA_A = c("A*01:01+A*68:01|A*01:02+A*68:55|A*01:99+A*68:66",
-              "A*02:01+A*03:01|A*02:02+A*03:03"),
-    HLA_B = c("B*07:02+B*58:01|B*07:03+B*58:09",
-              "B*08:01+B*15:01|B*08:02+B*15:17"),
+    HLA_A = c(
+      "A*01:01+A*68:01|A*01:02+A*68:55|A*01:99+A*68:66",
+      "A*02:01+A*03:01|A*02:02+A*03:03"
+    ),
+    HLA_B = c(
+      "B*07:02+B*58:01|B*07:03+B*58:09",
+      "B*08:01+B*15:01|B*08:02+B*15:17"
+    ),
     stringsAsFactors = FALSE
   )
 
