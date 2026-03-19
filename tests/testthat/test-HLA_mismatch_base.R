52f20b9dbc3c75fb59b85ac7a9e58c5c569d15fd
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2026-03-19 13:40:32 -0400

    Added test to HLA_mismatch_base

--- tests/testthat/test-HLA_mismatch_base.R
+++ tests/testthat/test-HLA_mismatch_base.R
@@ -13,8 +13,11 @@ test_that("HLA_mismatch_base correctly identifies mismatches", {
   result_GvH <- HLA_mismatch_base(GL_string_recip, GL_string_donor, loci, direction = "GvH")
   expect_equal(result_GvH, "HLA-A=HLA-A*02:01, HLA-B=HLA-B*08:01, HLA-DRB1=HLA-DRB1*07:01")
 
-  result_single_locus <- HLA_mismatch_base(GL_string_recip, GL_string_donor, "HLA-A", direction = "HvG")
-  expect_equal(result_single_locus, "HLA-A*01:01")
+  result_single_locus_HvG <- HLA_mismatch_base(GL_string_recip, GL_string_donor, "HLA-A", direction = "HvG")
+  expect_equal(result_single_locus_HvG, "HLA-A*01:01")
+
+  result_single_locus_GvH <- HLA_mismatch_base(GL_string_recip, GL_string_donor, "HLA-A", direction = "GvH")
+  expect_equal(result_single_locus_GvH, "HLA-A*02:01")
 
   result_HvG_homo1 <- HLA_mismatch_base(GL_string_recip, GL_string_donor, loci, direction = "HvG", homozygous_count = 1)
   expect_equal(result_HvG_homo1, "HLA-A=HLA-A*01:01, HLA-B=HLA-B*44:02, HLA-DRB1=HLA-DRB1*13:01")
