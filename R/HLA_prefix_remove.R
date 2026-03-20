e7041eae04dee8e8b54e9f3632de535abb82b043
k96nb01 <nicholas.brown@pennmedicine.upenn.edu>
2026-03-19 20:35:16 -0400

    Revised HLA_prefix_remove

--- R/HLA_prefix_remove.R
+++ R/HLA_prefix_remove.R
@@ -37,24 +37,22 @@ HLA_prefix_remove <- function(data, keep_locus = FALSE) {
   check_logical_flag(keep_locus, "keep_locus")
 
   # Removes any HLA and locus prefixes from typing results.
-  step_1 <- data %>%
+  result <- data %>%
     GLstring_expand_longer() %>%
-    # replaces "HLA-" with an empty string in each cell.
+    # Remove "HLA-" prefix from each allele.
     mutate(value = str_replace(value, "HLA-", ""))
 
-  if (keep_locus) {
-    step_2 <- step_1
-  } else {
-    # Remove the locus.
-    step_2 <- step_1 %>%
-      # replaces any sequences of alphabetic characters at the start of the string with an empty string
+  if (!keep_locus) {
+    # Remove locus prefix in two passes:
+    # Pass 1: Remove alphabetic prefix anchored at the start (e.g. "DRB" from "DRB1*04:01", "A" from "A2").
+    # Pass 2: Remove any remaining digits followed by an asterisk (e.g. "1*" from "1*04:01").
+    # These cannot be combined because pass 2 must be unanchored to catch the asterisk
+    # after pass 1 has already stripped the alpha characters.
+    result <- result %>%
       mutate(value = str_replace(value, "^[:alpha:]+", "")) %>%
-      # replaces any sequences of digits followed by an asterisk with an empty string
       mutate(value = str_replace(value, "[:digit:]*\\*", ""))
   }
 
-  # Reassemble the GL string
-  ambiguity_table_to_GLstring(step_2)
+  # Reassemble the GL string.
+  ambiguity_table_to_GLstring(result)
 }
-
-globalVariables(c("step_1", "step_2"))
